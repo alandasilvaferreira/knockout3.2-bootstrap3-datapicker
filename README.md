71cd5d94aff193c27858a3a22994418e51d4951d
alandasilvaferreira <alan.da.silva.ferreira@hotmail.com>
2015-02-20 22:16:39 -0200

    kick-starter

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # knockout3.2-bootstrap3-datapicker
+
+Depends of 
+Bootstrap v3.3.2 - http://getbootstrap.com
+Knockout JavaScript library v3.2.0 - http://knockoutjs.com/
+bootstrap-datepicker - https://github.com/eternicode/bootstrap-datepicker/
+
+# Usage
+
+    <div data-bind="daterange: {start: observableStart, end: observableEnd}">
+        <input type="text"/>
+        <span class="input-group-addon">até</span>
+        <input type="text"/>
+    </div>
---
+++ knockout3-bootstrap3-datepiker.js
@@ -0,0 +1,32 @@
+﻿ko.bindingHandlers.daterange = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+        var accessor = valueAccessor();
+        var startObservable = accessor.start;
+        var endObservable = accessor.end;
+
+        $(element).datepicker({
+            format: "dd/mm/yyyy",
+            todayBtn: "linked",
+            autoclose: true,
+            todayHighlight: true
+        }).on("changeDate", function (e) {
+            startObservable(e.dates[0]);
+            endObservable(e.dates[1]);
+
+        });
+
+    },
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        if (escape_ko_bindingHandlers_dateinput) return;
+        var accessor = valueAccessor();
+        var startObservable = accessor.start;
+        var endObservable = accessor.end;
+
+        //if (ko.isObservable(observable)) {
+        //    var valuetoWrite = observable();
+        //    $(element).tagsinput('removeAll');;
+        //    $(element).tagsinput('items', valuetoWrite);
+        //}
+    }
+
+};
